7b9f4cab7b36759f84de3abc4a1078f3d637be01
santosgui06 <santosgui1212@gmail.com>
2026-05-09 21:33:59 -0300

    Gui passou aqui

--- src/projetointegradorfinal/ProjetoIntegradorFinal.java
+++ src/projetointegradorfinal/ProjetoIntegradorFinal.java
@@ -8,6 +8,7 @@ public class ProjetoIntegradorFinal {
     public static void main(String[] args) {
         System.out.println("Iniciando Projeto!!!");
         System.out.println("Teste well");
+        System.out.println("Teste gui");
     }
 
 }
