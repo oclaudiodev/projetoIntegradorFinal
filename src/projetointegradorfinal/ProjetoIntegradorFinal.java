e87a7e67a2424b2bce3a6c78761d7296c2df2614
oclaudiodev <7claudiosouza@gmail.com>
2026-05-13 01:07:32 -0300

    refatorandoCodigo+AjustesGerais+FuncaoAumentarMatrizContato

--- src/projetointegradorfinal/ProjetoIntegradorFinal.java
+++ src/projetointegradorfinal/ProjetoIntegradorFinal.java
@@ -8,120 +8,128 @@ import java.io.IOException;
 public class ProjetoIntegradorFinal {
 
     public static void main(String[] args) {
-        //AQUI PUXA TODAS AS FUNÇÕES!!!
         Scanner sc = new Scanner(System.in);
         int opcaoPrincipal = 0;
-        int opcaoCliente = 0;
-        int opcaoContato = 0;
-        int opcaoRelatorio = 0;
         int clientesTamanhoMatriz = 0;
         int contatosTamanhoMatriz = 0;
         String[][] clientes = new String[0][8];
         String[][] contatos = new String[0][5];
+
         do {
             opcaoPrincipal = menuPrincipal(sc);
             switch (opcaoPrincipal) {
                 case 0:
                     System.out.println("Encerrando Sistema!");
                     break;
-                case 1: //1 - Gerenciar clientes
-                    do {
-                        opcaoCliente = menuCliente(sc);
-                        if (opcaoCliente < 0 || opcaoCliente > 6) {
-                            System.out.println("Opção Inválida");
-                        } else if (opcaoCliente == 0) {
-                            System.out.println("Saindo da aba Clientes");
-                            System.out.println("");
-                        } else if (opcaoCliente == 1) {
-                            //1 - Incluir cliente
-                            clientes = aumentarMatrizClientes(clientes);
-                            incluirCliente(clientes, sc, clientesTamanhoMatriz);
-                            clientesTamanhoMatriz++;
-                        } else if (opcaoCliente == 2) {
-                            //2 - Listar clientes (todos os clientes)
-                            listarClientes(clientes, clientesTamanhoMatriz);
-
-                        } else if (opcaoCliente == 3) {
-                            //3 - Consultar cliente por código
-                            int codigo = buscarClientePorCodigo(clientes, sc);
-                        } else if (opcaoCliente == 4) {
-                            //4 - Alterar cliente
-
-                        } else if (opcaoCliente == 5) {
-                            //5 - Apagar cliente
-
-                        } else if (opcaoCliente == 6) {
-                            //6 - Ordenar por nome
-                        }
-                    } while (opcaoCliente != 0);
+                case 1:
+                    int[] resultadoClientes = gerenciarClientes(clientes, clientesTamanhoMatriz, sc);
+                    clientesTamanhoMatriz = resultadoClientes[0];
                     break;
-                case 2: //2 - Gerenciar contatos
-                    do {
-                        opcaoContato = menuContato(sc);
-                        if (opcaoContato < 0 || opcaoContato > 5) {
-                            System.out.println("Opção Inválida");
-                        } else if (opcaoContato == 0) {
-                            System.out.println("Saindo da aba Contatos");
-                            System.out.println("");
-                        } else if (opcaoContato == 1) {
-                            //1 - Incluir contato
-                        } else if (opcaoContato == 2) {
-                            //2 - Listar contatos (Todos os clientes)
-
-                        } else if (opcaoContato == 3) {
-                            //3 -  Listar contatos de um cliente
-
-                        } else if (opcaoContato == 4) {
-                            //4 - Alterar contato
-
-                        } else if (opcaoContato == 5) {
-                            //5 - Apagar contato
-                        }
-                    } while (opcaoContato != 0);
+                case 2:
+                    int[] resultadoContatos = gerenciarContatos(contatos, contatosTamanhoMatriz, clientes, clientesTamanhoMatriz, sc);
+                    contatosTamanhoMatriz = resultadoContatos[0];
                     break;
-                case 3: //3 - Relatórios
-                    do {
-                        opcaoRelatorio = menuRelatorio(sc);
-                        switch (opcaoRelatorio) {
-                            case 0:
-                                System.out.println("Saindo da aba de Relatorios");
-                                break;
-                            case 1:
-                                //  Listar cliente etotal de cliente por contato
-                                break;
-                            case 2:
-                                //  Sumarização de dados
-                                //o Total de cliente
-                                //o Total de contatos
-                                //o Contatos por cliente (média)
-                                //o Clientes sem contato
-                                break;
-                            default:
-                                System.out.println("Opção inválida... Por favor digite uma opcao valida.");
-                        }
-                    } while (opcaoRelatorio != 0);
+                case 3:
+                    gerenciarRelatorios(clientes, clientesTamanhoMatriz, contatos, contatosTamanhoMatriz, sc);
                     break;
                 case 4:
-                    
                     clientes = carregarClientesCSV();
                     contatos = carregarContatosCSV();
-
                     clientesTamanhoMatriz = clientes.length;
-
+                    contatosTamanhoMatriz = contatos.length;
                     System.out.println("Dados carregados com sucesso!");
                     break;
-                    
                 default:
                     System.out.println("Opção Inválida");
             }
         } while (opcaoPrincipal != 0);
-
     }
 
-    //FUNÇÕES
+    // Menu Gerenciar Clientes
+
+    public static int[] gerenciarClientes(String[][] clientes, int clientesTamanhoMatriz, Scanner sc) {
+        int opcaoCliente = 0;
+        do {
+            opcaoCliente = menuCliente(sc);
+            if (opcaoCliente < 0 || opcaoCliente > 6) {
+                System.out.println("Opção Inválida");
+            } else if (opcaoCliente == 0) {
+                System.out.println("Saindo da aba Clientes");
+                System.out.println("");
+            } else if (opcaoCliente == 1) {
+                clientes = aumentarMatrizClientes(clientes);
+                incluirCliente(clientes, sc, clientesTamanhoMatriz);
+                clientesTamanhoMatriz++;
+            } else if (opcaoCliente == 2) {
+                listarClientes(clientes, clientesTamanhoMatriz);
+            } else if (opcaoCliente == 3) {
+                buscarClientePorCodigo(clientes, sc);
+            } else if (opcaoCliente == 4) {
+                // alterarCliente
+            } else if (opcaoCliente == 5) {
+                // apagarCliente
+            } else if (opcaoCliente == 6) {
+                // ordenarClientesPorNome
+            }
+        } while (opcaoCliente != 0);
+        return new int[]{clientesTamanhoMatriz};
+    }
+
+        // Menu Gerenciar Clientes
+
+    public static int[] gerenciarContatos(String[][] contatos, int contatosTamanhoMatriz, String[][] clientes, int clientesTamanhoMatriz, Scanner sc) {
+        int opcaoContato = 0;
+        do {
+            opcaoContato = menuContato(sc);
+            if (opcaoContato < 0 || opcaoContato > 5) {
+                System.out.println("Opção Inválida");
+            } else if (opcaoContato == 0) {
+                System.out.println("Saindo da aba Contatos");
+                System.out.println("");
+            } else if (opcaoContato == 1) {
+                // incluirContato
+            } else if (opcaoContato == 2) {
+                // listarContatosTabela
+            } else if (opcaoContato == 3) {
+                // listarContatosPorCliente
+            } else if (opcaoContato == 4) {
+                // alterarContato
+            } else if (opcaoContato == 5) {
+                // apagarContato
+            }
+        } while (opcaoContato != 0);
+        return new int[]{contatosTamanhoMatriz};
+    }
+
+    public static void gerenciarRelatorios(String[][] clientes, int clientesTamanhoMatriz, String[][] contatos, int contatosTamanhoMatriz, Scanner sc) {
+        int opcaoRelatorio = 0;
+        do {
+            opcaoRelatorio = menuRelatorio(sc);
+            switch (opcaoRelatorio) {
+                case 0:
+                    System.out.println("Saindo da aba de Relatorios");
+                    break;
+                case 1:
+                    // Listar cliente e total de cliente por contato
+                    break;
+                case 2:
+                    // Sumarização de dados
+                    // Total de cliente
+                    // o Total de contatos
+                    // o Contatos por cliente (média)
+                    // o Clientes sem contato
+                    break;
+                default:
+                    System.out.println("Opção inválida... Por favor digite uma opcao valida.");
+            }
+        } while (opcaoRelatorio != 0);
+    }
+
+    // ========== FUNÇÕES ==========
+
     public static int pedirNumero(Scanner input) {
         int numero = input.nextInt();
-        input.nextLine(); // já limpa o Enter aqui
+        input.nextLine();
         return numero;
     }
 
@@ -136,8 +144,7 @@ public class ProjetoIntegradorFinal {
         System.out.println("3 - Relatórios");
         System.out.println("4 - Carregar dados de teste (CSV)");
         System.out.println("0 – Sair");
-        int opcao = pedirNumero(input);
-        return opcao;
+        return pedirNumero(input);
     }
 
     public static int menuCliente(Scanner input) {
@@ -148,8 +155,7 @@ public class ProjetoIntegradorFinal {
         System.out.println("5 - Apagar cliente");
         System.out.println("6 - Ordenar por nome");
         System.out.println("0 - Voltar");
-        int opcao = pedirNumero(input);
-        return opcao;
+        return pedirNumero(input);
     }
 
     public static int menuContato(Scanner input) {
@@ -159,20 +165,18 @@ public class ProjetoIntegradorFinal {
         System.out.println("4 - Alterar contato");
         System.out.println("5 - Apagar contato");
         System.out.println("0 - Voltar");
-        int opcao = pedirNumero(input);
-        return opcao;
+        return pedirNumero(input);
     }
 
     public static int menuRelatorio(Scanner input) {
         System.out.println("1 - Listar Clientes");
         System.out.println("2 - Sumarização de Dados");
         System.out.println("0 - Voltar");
-        int opcao = input.nextInt();
-        return opcao;
+        return pedirNumero(input);
     }
 
-    //Clientes
-    //aumentarMatrizClientes
+    // ========== CLIENTES ==========
+
     public static String[][] aumentarMatrizClientes(String[][] clientesAntiga) {
         int novaLinha = clientesAntiga.length + 1;
         String[][] novaMatrizClientes = new String[novaLinha][8];
@@ -184,7 +188,6 @@ public class ProjetoIntegradorFinal {
         return novaMatrizClientes;
     }
 
-    // incluirCliente
     public static void incluirCliente(String[][] incluirCliente, Scanner input, int limitadorMatriz) {
         for (int i = limitadorMatriz; i < incluirCliente.length; i++) {
             for (int j = 0; j < incluirCliente[i].length; j++) {
@@ -225,53 +228,56 @@ public class ProjetoIntegradorFinal {
         }
     }
 
-    //listarClientesTabela
-    public static void listarClientes(String[][] listarCliente, int tamanhoMatriz) {
-        System.out.printf("%-25.25s|%-25.25s|%-25.25s|%-25.25s|%-25.25s|%-25.25s|%-25.25s|%-25.25s\n", "Codigo", "Nome", "CPF/CNPJ", "Data de nascimento", "Sexo", "Cidade", "Estado", "Status");
-        for (int i = 0; i < tamanhoMatriz; i++) {
-            for (int j = 0; j < listarCliente[i].length; j++) {
-                System.out.printf("%-25.25s", listarCliente[i][j]);
-            }
-            System.out.println();
-            System.out.println();
-        }
+    // Função auxiliar: imprime cabeçalho e separador da tabela de clientes
+    public static void imprimirCabecalhoClientes() {
+        System.out.printf("%-7s | %-20s | %-15s | %-12s | %-5s | %-20s | %-7s | %-8s%n",
+                "Codigo", "Nome", "CPF/CNPJ", "Nascimento", "Sexo", "Cidade", "Estado", "Status");
+        System.out.println("--------+----------------------+-----------------+--------------+-------+----------------------+---------+---------");
     }
 
-    ////[...]
+    // Função auxiliar: imprime uma linha da tabela de clientes
+    public static void imprimirLinhaCliente(String[] linha) {
+        System.out.printf("%-7s | %-20s | %-15s | %-12s | %-5s | %-20s | %-7s | %-8s%n",
+                linha[0], linha[1], linha[2], linha[3],
+                linha[4], linha[5], linha[6], linha[7]);
+    }
 
-    // buscarClientePorCodigo
+    public static void listarClientes(String[][] listarCliente, int tamanhoMatriz) {
+        if (tamanhoMatriz == 0) {
+            System.out.println("Nenhum cliente cadastrado.");
+            return;
+        }
+        imprimirCabecalhoClientes();
+        for (int i = 0; i < tamanhoMatriz; i++) {
+            imprimirLinhaCliente(listarCliente[i]);
+        }
+        System.out.println();
+    }
+
     public static int buscarClientePorCodigo(String[][] cliente, Scanner input) {
-        System.out.println("Código: ");
+        System.out.print("Código: ");
         String n = input.next();
+        input.nextLine();
 
         if (n.equals("0")) {
             return -1;
         }
-        if (n != null && !n.isEmpty()) {//verifica se n esta vazio ou nulo
-            System.out.printf("%-25.25s|%-25.25s|%-25.25s|%-25.25s|%-25.25s|%-25.25s|%-25.25s|%-25.25s\n", "Codigo", "Nome", "CPF/CNPJ", "Data de nascimento", "Sexo", "Cidade", "Estado", "Status");
+        if (n != null && !n.isEmpty()) {
             for (int i = 0; i < cliente.length; i++) {
-
                 if (cliente[i][0] != null && n.equals(cliente[i][0])) {
-
-                    for (int j = 0; j < cliente[i].length; j++) {
-                        System.out.printf("%-25.25s", cliente[i][j]);
-                    }
-                    System.out.println("");
-                    System.out.println("");
+                    imprimirCabecalhoClientes();
+                    imprimirLinhaCliente(cliente[i]);
+                    System.out.println();
                     return i;
                 }
             }
         }
-
         System.out.println("Cliente não encontrado.");
         return -1;
     }
-    
-    
 
     // alterarCliente
-
-////[...]
+    ////[...]
 
     // apagarCliente
     ////[...]
@@ -279,17 +285,18 @@ public class ProjetoIntegradorFinal {
     // ordenarClientesPorNome
     ////[...]
 
+    // ========== CONTATOS ==========
 
-
-    //CONTATOS
-
-
-    //aumentarMatrizContatos
-    private static String[][] aumentarMatrizContatos(String[][] matrizContatos) {
-
-        return matrizContatos;
+    public static String[][] aumentarMatrizContatos(String[][] matrizContatos) {
+        int novaLinha = matrizContatos.length + 1;
+        String[][] novaMatriz = new String[novaLinha][5];
+        for (int i = 0; i < matrizContatos.length; i++) {
+            for (int j = 0; j < matrizContatos[i].length; j++) {
+                novaMatriz[i][j] = matrizContatos[i][j];
+            }
+        }
+        return novaMatriz;
     }
-    ////[...]
 
     //incluirContato
     ////[...]
@@ -306,6 +313,7 @@ public class ProjetoIntegradorFinal {
     //apagarContato
     ////[...]
 
+    // ========== AUXILIARES ==========
 
     //AUXILIARES
 
@@ -324,11 +332,13 @@ public class ProjetoIntegradorFinal {
     ///
     ///
     // ========== FUNÇÕES DE CARREGAMENTO DE DADOS ==========
-    
+
+
     /*
       Carrega clientes do arquivo clientes.csv
       Formato: codigo,nome,cpf_cnpj,data_nascimento,sexo,cidade,estado,status
      */
+
     private static String[][] carregarClientesCSV() {
         String arquivo = "clientes.csv";
         String[][] matrizClientes = new String[0][8];
@@ -337,7 +347,6 @@ public class ProjetoIntegradorFinal {
         try (BufferedReader br = new BufferedReader(new FileReader(arquivo))) {
             String linha;
             boolean primeiraLinha = true;
-            
             while ((linha = br.readLine()) != null) {
                 // Pula o cabeçalho
                 if (primeiraLinha) {
@@ -347,7 +356,6 @@ public class ProjetoIntegradorFinal {
                 
                 // Divide a linha por vírgula
                 String[] dados = linha.split(",");
-                
                 if (dados.length >= 8) {
                     // Aumenta a matriz em 1 linha
                     matrizClientes = aumentarMatrizClientes(matrizClientes);
@@ -362,21 +370,17 @@ public class ProjetoIntegradorFinal {
                     matrizClientes[indice][5] = dados[5].trim(); // cidade
                     matrizClientes[indice][6] = dados[6].trim(); // estado
                     matrizClientes[indice][7] = dados[7].trim().toUpperCase(); // status
-                    
                     contador++;
                 }
             }
-            
             System.out.println("✓ " + contador + " clientes foram carregados do arquivo.");
-            
         } catch (IOException e) {
             System.out.println("\n✗ Erro ao ler arquivo " + arquivo + ": " + e.getMessage());
             System.out.println("Certifique-se de que o arquivo existe no diretório do projeto.");
         }
-        
         return matrizClientes;
     }
-    
+   
     /*
       Carrega contatos do arquivo contatos.csv
       Formato: codigo_contato,codigo_cliente,tipo,valor,status
@@ -389,7 +393,6 @@ public class ProjetoIntegradorFinal {
         try (BufferedReader br = new BufferedReader(new FileReader(arquivo))) {
             String linha;
             boolean primeiraLinha = true;
-            
             while ((linha = br.readLine()) != null) {
                 // Pula o cabeçalho
                 if (primeiraLinha) {
@@ -399,33 +402,24 @@ public class ProjetoIntegradorFinal {
                 
                 // Divide a linha por vírgula
                 String[] dados = linha.split(",");
-                
                 if (dados.length >= 5) {
                     // Aumenta a matriz em 1 linha
                     matrizContatos = aumentarMatrizContatos(matrizContatos);
                     int indice = matrizContatos.length - 1;
-                    
                     // Preenche a nova linha com os dados do CSV
                     matrizContatos[indice][0] = dados[0].trim(); // codigo_contato
                     matrizContatos[indice][1] = dados[1].trim(); // codigo_cliente
                     matrizContatos[indice][2] = dados[2].trim(); // tipo
                     matrizContatos[indice][3] = dados[3].trim(); // valor
                     matrizContatos[indice][4] = dados[4].trim().toUpperCase(); // status
-                    
                     contador++;
                 }
             }
-            
             System.out.println("✓ " + contador + " contatos foram carregados do arquivo.");
-            
         } catch (IOException e) {
             System.out.println("\n✗ Erro ao ler arquivo " + arquivo + ": " + e.getMessage());
             System.out.println("Certifique-se de que o arquivo existe no diretório do projeto.");
         }
-        
         return matrizContatos;
     }
-     
-     
-
 }
